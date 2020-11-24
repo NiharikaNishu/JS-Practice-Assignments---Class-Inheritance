25be9fce543f48fe403f5e295452a76767dd34b8
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-11-24 22:26:43 +0530

    cmmt

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,11 @@
 
 // declare class
 
-
 // export class using module.exports
+import React from "react";
+import ReactDom from "react-dom";
+import shape from "./shape.js";
+
+class circle extends shape {
+  calculateArea() {}
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
 
 // export class using module.exports
+import "React ,{Component}" from React;
+import "react-dom" from ReactDOM;
+
+class shape extends Component {
+   drawShape(){
+
+   }
+   calculateArea(){
+     
+   }
+}
+
+export default shape;
