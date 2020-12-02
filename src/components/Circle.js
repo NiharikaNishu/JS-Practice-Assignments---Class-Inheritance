e5c77c50d9f1f9c432f4a67a0951e1a1bb68ba27
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-12-03 00:28:58 +0530

    commit

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -4,13 +4,16 @@
 
 // export class using module.exports
 import React from "react";
-import ReactDom from "react-dom";
 import Shape from "./Shape.js";
 
 class Circle extends Shape {
   calculateArea() {
     console.log("NewArea");
     return null;
+  }
+
+  render() {
+    return <div>HERE</div>;
   }
 }
 
--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,23 +1,21 @@
 // declare class
 
 // export class using module.exports
-import "React ,{Component}" from React;
-import "react-dom" from ReactDOM;
+import { Component } from "react";
 
 class Shape extends Component {
-   color = "red" ;
-   constructor(colorName){
-      this.color = colorName;
-   }
-    drawShape(){
-      console.log("this is drawShape");
-      return null;
-   }
-    calculateArea(){
-      console.log("this is calculateArea");
-     return null;
-   }
-   
+  constructor(colorName) {
+    super();
+    this.color = colorName;
+  }
+  drawShape() {
+    console.log("this is drawShape");
+    return null;
+  }
+  calculateArea() {
+    console.log("this is calculateArea");
+    return null;
+  }
 }
 
 export default Shape;
